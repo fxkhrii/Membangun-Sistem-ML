2f28668ba8dd6943fb8c1d7294b72e2a6d1e8120
Afif Fakhri <afiffakhri04@gmail.com>
2026-06-07 17:18:56 +0700

    Add files via upload

---
+++ Eksperimen_SML_Afif Fakhri Muhammad/preprocessing/automate_Afif Fakhri Muhammad.py
@@ -0,0 +1,38 @@
+def run_pipeline(input_path, output_path):
+
+    import pandas as pd
+    from sklearn.preprocessing import LabelEncoder, StandardScaler
+    from sklearn.impute import SimpleImputer
+
+    df = pd.read_csv(input_path)
+
+    # target cleaning
+    df = df.dropna(subset=['RainTomorrow'])
+    df['RainTomorrow'] = df['RainTomorrow'].astype(str).str.strip()
+    df = df[df['RainTomorrow'].isin(['Yes','No'])]
+    df['RainTomorrow'] = df['RainTomorrow'].map({'No':0,'Yes':1})
+
+    X = df.drop('RainTomorrow', axis=1)
+
+    num_cols = X.select_dtypes(include=['float64']).columns
+    cat_cols = X.select_dtypes(include=['object']).columns
+
+    X[num_cols] = SimpleImputer(strategy='median').fit_transform(X[num_cols])
+    X[cat_cols] = SimpleImputer(strategy='most_frequent').fit_transform(X[cat_cols])
+
+    for col in cat_cols:
+        X[col] = LabelEncoder().fit_transform(X[col])
+
+    X = pd.DataFrame(StandardScaler().fit_transform(X), columns=X.columns)
+
+    df_final = X.copy()
+    df_final['RainTomorrow'] = df['RainTomorrow'].values
+
+    df_final.to_csv(output_path, index=False)
+
+
+if __name__ == "__main__":
+    run_pipeline(
+        "dataset_raw/weatherAUS.csv",
+        "preprocessing/dataset_preprocessing/processed_data.csv"
+    )
